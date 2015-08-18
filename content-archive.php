94aaecc7f2740fd88cfc4651c73b62f328deaa56
BenSibley <ben@competethemes.com>
2015-08-18 10:29:02 -0400

    more styling

--- content-archive.php
+++ content-archive.php
@@ -5,15 +5,15 @@
 		<div class="post-container">
 			<div class='post-header'>
 				<?php hybrid_do_atomic( 'sticky_post_status' ); ?>
-				<h2 class='post-title'>
-					<a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
-				</h2>
 				<span class="post-date">
 					<?php
 					$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date('r') ) );
 					printf( __( 'Published %s', 'apex' ), $date );
 					?>
 				</span>
+				<h2 class='post-title'>
+					<a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a>
+				</h2>
 			</div>
 			<div class="post-content">
 				<?php ct_apex_excerpt(); ?>
