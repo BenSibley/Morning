cf9d32fce2b946e0d0a5349ba05712a8b908628c
BenSibley <ben@competethemes.com>
2015-08-21 08:25:10 -0400

    loading fonts and dequeueing Apex default Google Fonts

--- functions.php
+++ functions.php
@@ -11,9 +11,12 @@ function ct_unit_override_footer_text( $footer_text ) {
 function ct_morning_register_scripts(){
 
 	// register Google Fonts typefaces & styles
-	wp_register_style( 'ct-morning-google-fonts', '//fonts.googleapis.com/css?family=Montserrat:400,700');
+	wp_register_style( 'ct-morning-google-fonts', '//fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700');
+
+	// dequeue Apex Google Fonts
+	wp_dequeue_style('ct-apex-google-fonts');
 
 	// enqueue Google Fonts
 	wp_enqueue_style('ct-morning-google-fonts');
 }
-add_action('wp_enqueue_scripts', 'ct_morning_register_scripts' );
+add_action('wp_enqueue_scripts', 'ct_morning_register_scripts', 99 );
