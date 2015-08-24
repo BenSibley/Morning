3d26939eca7061b611d20f7531a4176ff91eea46
BenSibley <ben@competethemes.com>
2015-08-24 08:39:09 -0400

    removed font default code to put it in Apex Pro isntead

--- functions.php
+++ functions.php
@@ -20,12 +20,3 @@ function ct_morning_register_scripts(){
 	wp_enqueue_style('ct-morning-google-fonts');
 }
 add_action('wp_enqueue_scripts', 'ct_morning_register_scripts', 99 );
-
-function ct_morning_update_customizer( $wp_customize ) {
-
-	$wp_customize->get_setting( 'heading_font' )->default = 'Montserrat';
-	$wp_customize->get_control( 'heading_font' )->description = __( 'Default font is Montserrat.', 'morning' );
-	$wp_customize->get_setting( 'site_title_font' )->default = 'Montserrat';
-	$wp_customize->get_control( 'site_title_font' )->description = __( 'Default font is Montserrat.', 'morning' );
-}
-add_action( 'customize_register', 'ct_morning_update_customizer', 90);
